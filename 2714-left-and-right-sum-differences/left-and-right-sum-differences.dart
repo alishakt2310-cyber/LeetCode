ab708070826aa9d71c9838dcfd724fc22a7ab884
alishakt2310-cyber <alishakt2310@gmail.com>
2026-05-21 10:42:16 +0530

    Time: 15 ms (50.00%) | Memory: 148.3 MB (100.00%) - LeetSync

---
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.dart
@@ -0,0 +1,20 @@
+class Solution {
+  List<int> leftRightDifference(List<int> nums) {
+    List<int> answer = [];
+
+for (int i = 0; i < nums.length; i++) {
+
+  int leftSum = 0;
+
+  for (int j = 0; j < i; j++) {
+    leftSum += nums[j];
+  }
+    int rightSum = 0;
+  for (int j = i + 1; j < nums.length; j++) {
+    rightSum += nums[j];
+  }
+  answer.add((leftSum - rightSum).abs());
+}
+return answer;
+  }
+}
