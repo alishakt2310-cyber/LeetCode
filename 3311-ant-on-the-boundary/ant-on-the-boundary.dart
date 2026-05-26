bda68398bbc8ec8e0ee38db5b593775cfeb5c43b
alishakt2310-cyber <alishakt2310@gmail.com>
2026-05-26 10:18:17 +0530

    Time: 373 ms (50.00%) | Memory: 148.7 MB (50.00%) - LeetSync

---
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int returnToBoundaryCount(List<int> nums) {
+    int position=0;
+int count=0;
+for(int i=0;i<nums.length;i++){
+  position += nums[i];
+
+if(position == 0){
+    count++;
+}
+  }
+
+return count;
+    
+  }
+}
